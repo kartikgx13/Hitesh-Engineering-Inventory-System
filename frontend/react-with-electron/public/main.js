9ae6e66afea995b368bb138e19928a4d43a2d01b
kartikgx13 <108775715+kartikgx13@users.noreply.github.com>
2024-04-29 21:07:04 +0530

    kartik first commit added electron code and horizontal nav

---
+++ frontend/react-with-electron/public/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    fullscreen:true,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  
+  mainWindow.removeMenu()
+  mainWindow.loadURL("http://localhost:3000")
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
---
+++ frontend/react-with-electron/src/components/HorizontalNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HorizontalNav() {
+  return (
+    <div className='w-full h-1/10 bg-sky-500 p-4 m-0'>
+        <div className='h-full bg-white text-1xl p-1 flex justify-center items-center w-20 rounded-sm'>
+            Hitesh Engg. Works
+        </div>
+    </div>
+  )
+}
+
+export default HorizontalNav
